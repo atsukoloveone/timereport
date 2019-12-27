ebbc168553486ed503fc175f097f4db7bc26cb87
Atsuko Yamazaki <atsuko@loveone.se>
2019-12-27 12:00:57 +0100

    Created a test file

---
+++ frontend/components/Link.react.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// Linkの実体は<span>~</span>もしくは<a>~</a>
+const STATUS = {
+  HOVERED: "hovered",
+  NORMAL: "normal",
+  NORMAL1: "normal1",
+};
+export default class Link extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.onMouseEnter = this.onMouseEnter.bind(this);
+    // this.onMouseLeave = this.onMouseLeave.bind(this);
+
+    this.state = {
+      class: STATUS.NORMAL,
+    };
+  }
+
+  // onMouseEnter()  {
+  onMouseEnter = () => {
+    this.setState({ class: STATUS.HOVERED });
+  };
+
+  // onMouseLeave()  {
+  onMouseLeave = () => {
+    this.setState({ class: STATUS.NORMAL1 });
+  };
+
+  render() {
+    return (
+      <a
+        className={this.state.class}
+        href={this.props.page || "#"}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        {this.props.children}
+      </a>
+    );
+  }
+}
+Link.propTypes = {
+  page: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
